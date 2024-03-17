0fe9fc040582c0a03bc2b00f5b26119b3c798758
akshayvakil <157507818+akshayvakil@users.noreply.github.com>
2024-03-18 00:43:23 +0530

    Validate complex json Fetch specific fields in array using for loop validate response.

--- src/test/java/APIAutomation/JiraTestSessionValidateComplexJson.java
+++ src/test/java/APIAutomation/JiraTestSessionValidateComplexJson.java
@@ -2,12 +2,15 @@ package APIAutomation;
 
 import io.restassured.RestAssured;
 import io.restassured.filter.session.SessionFilter;
+import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
 
 import java.io.File;
 
-public class JiraTestSessionIntegratePathandQueryParam {
+import org.testng.Assert;
+
+public class JiraTestSessionValidateComplexJson {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,18 +18,27 @@ public class JiraTestSessionIntegratePathandQueryParam {
 		RestAssured.baseURI = "http://localhost:8080";
 		SessionFilter sessionfilter = new SessionFilter();
 //Step1 -Login
-		given().header("Content-Type", "application/json")
+// User 
+		given().relaxedHTTPSValidation().header("Content-Type", "application/json")
 				.body("{ \"username\": \"JiraUser1\", \"password\": \"Jira123#\" }").log().all().filter(sessionfilter)
 				.when().post("/rest/auth/1/session").then().extract().response().asString();
 
-//Step2-Update Comment		
-		given().pathParam("id", "10007").log().all().header("Content-Type", "application/json")
-				.body("{\r\n" + "    \"body\": \"This is my  comment with attachment from REST API semper.\",\r\n"
-						+ "    \"visibility\": {\r\n" + "        \"type\": \"role\",\r\n"
-						+ "        \"value\": \"Administrators\"\r\n" + "    }\r\n" + "}")
-				.filter(sessionfilter).when().post("rest/api/2/issue/{id}/comment").then().log().all().statusCode(201);
+//Step2-Add Comment	
 		/// rest/api/2/issue/{id}/comment --> anything with {} is path parameter for
 		/// rest assured id=10007 will be fetched form abovepathParam method
+		
+ String expectedMessage="Hi How are you?";
+		String addCommentResponse = given().pathParam("id", "10007").log().all()
+				.header("Content-Type", "application/json")
+				.body("{\r\n" + "    \"body\": \""+expectedMessage+"\",\r\n"
+						+ "    \"visibility\": {\r\n" + "        \"type\": \"role\",\r\n"
+						+ "        \"value\": \"Administrators\"\r\n" + "    }\r\n" + "}")
+				.filter(sessionfilter).when().post("rest/api/2/issue/{id}/comment").then().log().all().statusCode(201)
+				.extract().response().asString();
+
+		JsonPath js = new JsonPath(addCommentResponse);
+		String commentID = js.getString("id");
+		// First GOAL: This comment id newly created should be present in response
 
 //Step3-Update Comment with attachments
 		/**
@@ -44,6 +56,7 @@ public class JiraTestSessionIntegratePathandQueryParam {
 				.header("Content-Type", "multipart/form-data").multiPart("file", new File("JiraAttachment.txt")).when()
 				.post("rest/api/2/issue/{id}/attachments").then().log().all().assertThat().statusCode(200);
 //Step4: Get Issue
+// Validate newly added comments
 		/**
 		 * https://docs.atlassian.com/software/jira/docs/api/REST/9.14.0/#api/2/issue-getIssue
 		 * /rest/api/2/issue/{issueIdOrKey}
@@ -51,11 +64,36 @@ public class JiraTestSessionIntegratePathandQueryParam {
 		 */
 		String resonseIssuedetails = given().filter(sessionfilter).pathParam("id", "10007")
 				.queryParam("fields", "comment").log().all().when().get("rest/api/2/issue/{id}").then().log().all()
+				// .query param will give only comments response; instead of giving complete
+				// json
+
 				.extract().response().asString();
 
 		System.out.println("================================");
 
 		System.out.println(resonseIssuedetails);
+		// We want to validate only new comments
+		// Create json path class, apply for loop and filter
+
+		JsonPath js1 = new JsonPath(resonseIssuedetails);		
+		////Get count of comments in array use getInt not only get
+		int commentsCount =js1.getInt("fields.comment.comments.size()");
+		//iterate each and every array of comments
+		//commenntID added above should be present in response below
+		for (int i=0;i<commentsCount;i++)
+		{
+			//to print only comment id 
+			//System.out.println(js1.get("fields.comment.comments["+i+"].id"));
+			String CommentIdissue= js1.get("fields.comment.comments["+i+"].id").toString();
+			if (CommentIdissue.equalsIgnoreCase(commentID)) 
+			{
+				String message=js1.get("fields.comment.comments["+i+"].body").toString();
+				Assert.assertEquals(message, expectedMessage);
+				
+			}
+		}
+
 	}
 
 }
+ 
